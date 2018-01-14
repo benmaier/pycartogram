02fbaf203b86504a79ee606ca2c8bde0f2a114b3
Benjamin Maier <benjaminfrankmaier@gmail.com>
2018-01-14 23:40:25 +0100

    version up

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pycartogram',
-      version='0.0.2',
+      version='0.0.3',
       description='Collection of tools to effeciently generate cartograms as developed by Gastner and Newman.',
       url='https://github.com/benmaier/pycartogram',
       author='Benjamin F. Maier',
